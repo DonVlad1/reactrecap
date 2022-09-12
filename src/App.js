a25ab51bb4a6f1e4b1d618f2f8f0facf201b7683
Vlad <vladut_stoian@live.co.uk>
2022-09-12 13:05:39 +0100

    Working Movie Card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import SearchIcon from "../src/search.svg"
+import MovieCard from "./components/MovieCard";
 
 //api key:  fa036fab
 
@@ -8,6 +9,7 @@ const API_URL = 'http://www.omdbapi.com?apikey=fa036fab'
 const App = () =>
 {
   const [searchTerm, setSearchTerm] = useState("")
+  const [movies, setMovies] = useState([])
 
   useEffect(() =>
   {
@@ -19,6 +21,7 @@ const App = () =>
     const req = await fetch(`${API_URL}&s=${title}`)
     const res = await req.json()
     console.log(res.Search)
+    setMovies(res.Search)
   }
 
   return (
@@ -26,9 +29,21 @@ const App = () =>
       <h1>My Movie App</h1>
       <div className="search">
         <input placeholder='Search for a film' value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
-        <img src={SearchIcon} alt="search" onClick={() => searchFilms(searchTerm)}
-        />
+        <img src={SearchIcon} alt="search" onClick={() => searchFilms(searchTerm)} />
       </div>
+      {movies?.length > 0 ?
+        (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie}></MovieCard>
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No Movies to Display</h2>
+          </div>
+        )
+      }
     </div>
   )
 }
